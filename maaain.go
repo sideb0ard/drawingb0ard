f547b7a5b461dad8ff19ee0a73f87f385b830b6b
Thorsten Sideboard <thorsten.sideb0ard@gmail.com>
2020-12-07 22:33:37 -0800

    fixed teh curves!

--- maaain.go
+++ maaain.go
@@ -44,17 +44,10 @@ func AdjustPoint(point library.Point, x int, y int) library.Point {
 }
 
 func DrawCurvedLine(x int, y int, squig []library.Point, color string) {
-	//fmt.Printf("Curved! x:%d y:%d\n", x, y)
-	// xpos, ypos := library.SplitPointsSlice(squig)
-	// tx := Map(xpos, func(val int) int { return val + x })
-	// ty := Map(ypos, func(val int) int { return val + y })
-
-	//fmt.Printf("X:%d Y:%d\n", x, y)
 	for i := 0; i < len(squig); i++ {
 
-		// fmt.Printf("Start is x:%f y:%f\n", squig[i].X, squig[i].Y)
 		start_point := AdjustPoint(squig[i], x, y)
-		// fmt.Printf("Start_POINT is x:%f y:%f\n", start_point.X, start_point.Y)
+
 		var previous library.Point
 		var next library.Point
 		// Start Control Point
@@ -64,29 +57,18 @@ func DrawCurvedLine(x int, y int, squig []library.Point, color string) {
 		if i < len(squig)-1 {
 			next = AdjustPoint(squig[i+1], x, y)
 		}
-
-		//fmt.Println("START CONTROL:")
 		start_control := library.ControlPoint(start_point, previous, next, false)
 
-		// end control point
-		var ecurrent library.Point
-		var enext library.Point
+		end_point := start_point // default
 
-		if i < len(squig)-3 {
-			ecurrent = AdjustPoint(squig[i+1], x, y)
-			enext = AdjustPoint(squig[i+2], x, y)
-		} else if i < len(squig)-2 {
-			ecurrent = AdjustPoint(squig[i+1], x, y)
-		} else {
-			ecurrent = start_point
-		}
-
-		//fmt.Println("END CONTROL:")
-		end_control := library.ControlPoint(ecurrent, start_point, enext, true)
-		end_point := AdjustPoint(squig[i], x, y)
-		if i < len(squig)-1 {
+		var end_next library.Point
+		if i < len(squig)-2 {
+			end_point = AdjustPoint(squig[i+1], x, y)
+			end_next = AdjustPoint(squig[i+2], x, y)
+		} else if i < len(squig)-1 {
 			end_point = AdjustPoint(squig[i+1], x, y)
 		}
+		end_control := library.ControlPoint(end_point, start_point, end_next, true)
 
 		canvas.Bezier(int(start_point.X), int(start_point.Y), int(start_control.X), int(start_control.Y), int(end_control.X), int(end_control.Y), int(end_point.X), int(end_point.Y), "fill:none; stroke:"+color)
 	}
